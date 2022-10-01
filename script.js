78d3108b92988ad21f623ecbdefc87cafb458792
Chris Gibson <chrischarlesgibson@gmail.com>
2022-10-01 15:20:58 -0500

    add basic HTML and CSS skeletons

---
+++ script.js
@@ -0,0 +1,25 @@
+//pseudo code//
+/*
+-go to page and you will see start page with button that says take quiz. :there should be a 'view highscores" links at top left and timer at top right.)
+
+-click start quiz button and the graphics on the main page disappear and a page showing a multiple choice question shows up. also the click event will start the timer countdown
+
+-multiple choice question will have 4 buttons for the 4 options you can select.
+
+-if you select wrong answer, message shows up saying "wrong" and a certain ammount of time is taking off the timer.then it autmatically goes to next question
+
+
+- if you select  correct answer,
+message shows up saying "correct" and a no time is taken off timer. then it autmatically goes to next question
+
+-when all questions are answered or timer runs out. text disapeears and new text appears says 'all done" and tells you your score and lets you enter you intials so that you can store your highscore. 
+
+
+
+-once enter initals, new display shows with high scores listed and intials.on this page there should be a button to (optional but maybe need it:clear highscores and a button to go back to start page). it should store your highscore so dont necessaryily need it to show other peoples highscores.
+
+
+
+
+
+*/
